01437abd2980c2215af1dfc7955a338216cfb46e
Funny <slepon@yandex.ru>
2026-07-18 18:45:50 +0300

    Add JSON file persistence

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,6 +2,9 @@ package storage
 
 import (
 	"bookmark-keeper/internal/models"
+	"encoding/json"
+	"log"
+	"os"
 	"sync"
 	"time"
 )
@@ -13,10 +16,13 @@ type Storage struct {
 }
 
 func NewStorage() *Storage {
-	return &Storage{
+	s := &Storage{
 		bookmarks: make(map[int]models.Bookmark),
 		nextID:    1,
 	}
+
+	s.load()
+	return s
 }
 
 func (s *Storage) Create(url, title string, tags []string) models.Bookmark {
@@ -39,6 +45,7 @@ func (s *Storage) Create(url, title string, tags []string) models.Bookmark {
 
 	s.nextID++
 	s.bookmarks[id] = createMark
+	s.save()
 	return createMark
 }
 
@@ -71,5 +78,51 @@ func (s *Storage) Delete(id int) bool {
 		return false
 	}
 	delete(s.bookmarks, id)
+	s.save()
 	return true
+}
+
+func (s *Storage) save() {
+	saveBookmarks := make([]models.Bookmark, 0)
+	for _, bookmark := range s.bookmarks {
+		saveBookmarks = append(saveBookmarks, bookmark)
+	}
+
+	data, err := json.Marshal(saveBookmarks)
+	if err != nil {
+		log.Println("Ошибка сериализации")
+		return
+	}
+
+	if err := os.WriteFile("bookmarks.json", data, 0644); err != nil {
+		log.Println("Ошибка записи")
+		return
+	}
+}
+
+func (s *Storage) load() {
+	data, err := os.ReadFile("bookmarks.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return
+		}
+		log.Printf("ошибка чтения файла: %v", err)
+		return
+	}
+
+	var loaded []models.Bookmark
+	if err := json.Unmarshal(data, &loaded); err != nil {
+		log.Printf("ошибка парсинга: %v", err)
+		return
+	}
+
+	maxID := 0
+
+	for _, b := range loaded {
+		s.bookmarks[b.ID] = b
+		if b.ID > maxID {
+			maxID = b.ID
+		}
+	}
+	s.nextID = maxID + 1
 }
